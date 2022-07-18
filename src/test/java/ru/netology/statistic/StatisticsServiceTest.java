0247658847d29f15b01801a9295bf3dc0955a24c
Pankeeva Olga <0lga.pankeeva@mail.ru>
2022-07-18 07:19:13 +0400

    commit test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -18,5 +18,16 @@ public class StatisticsServiceTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void findMaxIncome() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 32, 6, 11, 11, 12};
+        long expected = 32;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 
 }
